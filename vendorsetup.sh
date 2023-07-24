eebaf352a9b60977fcc5328e2d870eceaf07f707
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-07-24 13:08:58 +0530

    RMX1901:refactor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
 #Syncing KernelSU to latest source and avoid a conflict
-echo "Syncing KernelSU to latest source [3/4]"
+echo "Syncing KernelSU to latest source [1/2]"
 cd kernel/realme/RMX1901 && git submodule sync && git submodule update --init --recursive && cd KernelSU && git checkout main && git pull && touch Android.mk && cd ../../../..
+
+echo 'Patching Realme In-Screen Proximity [2/2]'
+# Realme In-Screen Proximity Patch
+cd frameworks/base && git fetch git@github.com:H1mJT/proximity_patch.git && git cherry-pick afb38a0f9f9a612b13b12b901ab9b7f0fffae3de && cd ../..
